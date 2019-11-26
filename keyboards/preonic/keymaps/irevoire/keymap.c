6f048517a471f5d44f8f2542c716c28bd6155c14
Thomas Campistron <irevoire@hotmail.fr>
2019-11-26 19:35:59 +0100

    move the music mode to another layer and add a numpad on the left hand

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -56,21 +56,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Brigh+|  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  | Lock |  NO  | Vol+ |
+ * |Brigh+|  NO  |   7  |   8  |   9  |  NO  |  NO  |  NO  |  NO  | Lock |  NO  | Vol+ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |Brigh-|  NO  |  NO  |  NO  |  NO  |  NO  |  NO  | Left | Down |  Up  |Right | Vol- |
+ * |Brigh-|  NO  |   4  |   5  |   6  |  NO  |Caplok| Left | Down |  Up  |Right | Vol- |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Shift |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
+ * |Shift |  .   |   3  |   2  |   1  |   0  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |TG(META)|Ctrl| Alt  |Super |Lower |    Space    |Raise | Left | Down |  Up  |  NO  |
+ * |TG(META)|Ctrl| Alt  |Super |Lower |    Space    |Enter |Option|  NO  |  NO  |Escape|
  * `-----------------------------------------------------------------------------------'
  */
 [_ARROW] = LAYOUT_preonic_grid( \
-  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5  ,   KC_F6  ,   KC_F7  ,   KC_F8  ,   KC_F9,     KC_F10,    KC_F11,    KC_F12,   \
-  KC_BRIU,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
-  KC_BRID,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
-  _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
-  TG(_META), _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   MU_TOG    \
+  KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,   KC_F10,    KC_F11,    KC_F12,   \
+  KC_BRIU,   XXXXXXX,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
+  KC_BRID,   XXXXXXX,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
+  _______,   KC_PDOT,   KC_P3,     KC_P2,     KC_P1,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
+  TG(_META),_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
 ),
 
 /* Meta
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | TRNS |  NO  |  NO  |  NO  |  NO  |      NO     |  NO  |  NO  |  NO  |  NO  |RESET |
+ * | TRNS |  NO  |  NO  |  NO  |  NO  |      NO     |  NO  |  NO  |  NO  |RESET |MUSIC |
  * `-----------------------------------------------------------------------------------'
  */
 [_META] = LAYOUT_preonic_grid( \
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   RESET     \
+  _______,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,     RESET,   MU_TOG    \
 ),
 
 };
