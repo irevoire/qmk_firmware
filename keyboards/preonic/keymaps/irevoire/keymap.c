61abebd5ea4a27abe721036b5afdf4a5f6c03ba1
Thomas Campistron <irevoire@hotmail.fr>
2019-11-26 19:48:42 +0100

    move the brightness keys one row to the right so the delete and tab key are accessible even on layer 1

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -56,9 +56,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Brigh+|  NO  |   7  |   8  |   9  |  NO  |  NO  |  NO  |  NO  | Lock |  NO  | Vol+ |
+ * | Tab  |Brigh+|   7  |   8  |   9  |  NO  |  NO  |  NO  |  NO  | Lock |  NO  | Vol+ |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |Brigh-|  NO  |   4  |   5  |   6  |  NO  |Caplok| Left | Down |  Up  |Right | Vol- |
+ * | Del  |Brigh-|   4  |   5  |   6  |  NO  |Caplok| Left | Down |  Up  |Right | Vol- |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |  .   |   1  |   2  |   3  |   0  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ARROW] = LAYOUT_preonic_grid( \
   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,   KC_F10,    KC_F11,    KC_F12,   \
-  KC_BRIU,   XXXXXXX,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
-  KC_BRID,   XXXXXXX,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
+  _______,   KC_BRIU,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
+  _______,   KC_BRID,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
   _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
   TG(_META),_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
 ),
