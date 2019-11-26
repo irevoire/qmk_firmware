60116f0c703b4d7bf49374b0befb700372b0fab1
Thomas Campistron <irevoire@hotmail.fr>
2019-11-26 19:39:55 +0100

    fix the numpads

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |Brigh-|  NO  |   4  |   5  |   6  |  NO  |Caplok| Left | Down |  Up  |Right | Vol- |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |Shift |  .   |   3  |   2  |   1  |   0  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
+ * |Shift |  .   |   1  |   2  |   3  |   0  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |TG(META)|Ctrl| Alt  |Super |Lower |    Space    |Enter |Option|  NO  |  NO  |Escape|
  * `-----------------------------------------------------------------------------------'
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,   KC_F10,    KC_F11,    KC_F12,   \
   KC_BRIU,   XXXXXXX,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
   KC_BRID,   XXXXXXX,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
-  _______,   KC_PDOT,   KC_P3,     KC_P2,     KC_P1,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
+  _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
   TG(_META),_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
 ),
 
