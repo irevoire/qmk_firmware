d649361c22b09da158c4c829beecfccb27842aea
Thomas Campistron <irevoire@hotmail.fr>
2019-11-30 23:32:37 +0100

    add a gaming mode with direct acces to escape, backspace and the arrows

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -20,12 +20,14 @@
 enum preonic_layers {
   _BASE,
   _ARROW,
+  _GAME,
   _META,
 };
 
 enum preonic_keycodes {
   BASE = SAFE_RANGE,
   ARROW,
+  GAME,
   META,
 };
 
@@ -69,8 +71,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,   KC_F10,    KC_F11,    KC_F12,   \
   _______,   KC_BRIU,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
   _______,   KC_BRID,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
-  _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
+  _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX, TG(_GAME),   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
   TG(_META),_______,  _______,   _______,   _______,   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
+),
+
+/* Gaming
+ * ,-----------------------------------------------------------------------------------.
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |  Up  |   O  |   P  |  #   |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Del  |   A  |   S  |   D  |   F  |   G  |   H  | Left | Down |Right |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |  =   | Ctrl | Alt  |Super |  NO  |    Space    |Enter | Alt  |  [   |TG(GAM)|C(Esc)|
+ * `-----------------------------------------------------------------------------------'
+ */
+[_GAME] = LAYOUT_preonic_grid( \
+  KC_ESC,    KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,        \
+  KC_TAB,    KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_NUHS,        \
+  KC_BSPC,   KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_LEFT,    KC_DOWN, KC_RIGHT,KC_SCLN, KC_QUOT,        \
+  KC_LSFT,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        \
+  KC_EQL,    KC_LCTL,   KC_LALT, KC_LGUI,_______,  KC_SPC,  KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, TG(_GAME), LCTL_T(KC_ESC)  \
 ),
 
 /* Meta
