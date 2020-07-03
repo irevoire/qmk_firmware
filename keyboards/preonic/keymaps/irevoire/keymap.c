c61ceffcf18a21ca252bfe3bb60ce7d3142cb49e
Thomas Campistron <irevoire@hotmail.fr>
2020-07-03 21:42:46 +0200

    remove some useless functions

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NUHS,        \
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        \
-  KC_EQL,  KC_LCTL, KC_LALT, KC_LGUI,TT(_ARROW),KC_SPC, KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, KC_RBRC, LCTL_T(KC_ESC)  \
+  KC_EQL,  KC_LCTL, KC_LALT, KC_LGUI,MO(_ARROW),KC_SPC, KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, KC_RBRC, LCTL_T(KC_ESC)  \
 ),
 
 /* Arrow
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |  .   |   1  |   2  |   3  |   0  |  NO  |  NO  |  NO  |  NO  |  NO  | Mute |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |TG(META)|Ctrl| Alt  |Super |Lower |    Space    |Enter |Option|  NO  |  NO  |Escape|
+ * |   =  | Ctrl | Alt  |Super | BASE |    Space    |Enter |Option|  NO  |  NO  |Escape|
  * `-----------------------------------------------------------------------------------'
  */
 [_ARROW] = LAYOUT_preonic_grid( \
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,   KC_BRIU,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
   _______,   KC_BRID,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
   _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX, TG(_GAME),   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
-  TG(_META),_______,  _______,   _______, TG(_BASE),   _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
+  _______,   _______,  _______,   _______, TO(_BASE),  _______,   _______,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
 ),
 
 /* Gaming
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,    KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_NUHS,        \
   KC_BSPC,   KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_LEFT,    KC_DOWN, KC_RIGHT,KC_SCLN, KC_QUOT,        \
   KC_LSFT,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        \
-  KC_EQL,    KC_LCTL,   KC_LALT, KC_LGUI,_______,  KC_SPC,  KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, TG(_GAME), LCTL_T(KC_ESC)  \
+  KC_EQL,    KC_LCTL,   KC_LALT, KC_LGUI,_______,  KC_SPC,  KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, TO(_BASE), LCTL_T(KC_ESC)  \
 ),
 
 /* Meta
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  TG(_META),   KC_LCTL,   KC_LALT,   KC_LGUI,   XXXXXXX,   XXXXXXX,   XXXXXXX,     KC_UP,   KC_DOWN,   XXXXXXX,     RESET,   MU_TOG    \
+  TO(_META),   KC_LCTL,   KC_LALT,   KC_LGUI,   XXXXXXX,   XXXXXXX,   XXXXXXX,     KC_UP,   KC_DOWN,   XXXXXXX,     RESET,   MU_TOG    \
 ),
 
 };
