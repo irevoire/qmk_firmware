1b90f6489b74eb9110c03794260efd07db2b7c4f
irevoire <irevoire@hotmail.fr>
2020-07-09 02:36:52 +0200

    clean up some shit and start working on a fake azerty to bépo translation

--- keyboards/preonic/keymaps/irevoire/keymap.c
+++ keyboards/preonic/keymaps/irevoire/keymap.c
@@ -18,17 +18,15 @@
 #include "muse.h"
 
 enum preonic_layers {
-  _BASE,
+  _BEPO,
+  _AZERTY,
   _ARROW,
-  _GAME,
-  _META,
 };
 
 enum preonic_keycodes {
-  BASE = SAFE_RANGE,
+  BEPO = SAFE_RANGE,
+  AZERTY,
   ARROW,
-  GAME,
-  META,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -43,15 +41,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  =   | Ctrl | Alt  |Super |TT(Arr)|   Space    |Enter | Alt  |  [   |  ]   |C(Esc)|
+ * |  =   | Ctrl | Alt  |Super |MO(Arr)|   Space    |Enter | Alt  |  [   |  ]   |C(Esc)|
  * `-----------------------------------------------------------------------------------'
  */
-[_BASE] = LAYOUT_preonic_grid( \
+[_BEPO] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,        \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_NUHS,        \
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        \
   KC_EQL,  KC_LCTL, KC_LALT, KC_LGUI,MO(_ARROW),KC_SPC, KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, KC_RBRC, LCTL_T(KC_ESC)  \
+),
+
+[_AZERTY] = LAYOUT_preonic_grid( \
+  KC_GRV,  KC_3,    KC_1,    KC_2,    KC_5, KC_MINS,    KC_6,    KC_7,    KC_6,    KC_9,    KC_BSLS,   KC_EQL,        \
+  KC_TAB,  KC_B,    KC_2,    KC_P,    KC_O,    KC_7, KC_LBRC,    KC_V,    KC_D,    KC_L,       KC_J,     KC_9,        \
+  KC_BSPC, KC_Q,    KC_U,    KC_I,    KC_E,    KC_M,    KC_C,    KC_T,    KC_S,    KC_R,       KC_N,  KC_SCLN,        \
+  KC_LSFT, KC_0,    KC_Y,    KC_X, KC_COMM,    KC_K, KC_QUOT,    KC_A,    KC_G,    KC_H,       KC_F,  KC_RSFT,        \
+  KC_EQL,  KC_LCTL, KC_LALT, KC_LGUI,MO(_ARROW),KC_SPC, KC_SPC,  KC_ENT,  KC_RALT, KC_W,       KC_Z,  LCTL_T(KC_ESC)  \
 ),
 
 /* Arrow
@@ -71,50 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,   KC_F10,    KC_F11,    KC_F12,   \
   _______,   KC_BRIU,   KC_P7,     KC_P8,     KC_P9,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_LOCK,   XXXXXXX,   KC_VOLU,  \
   _______,   KC_BRID,   KC_P4,     KC_P5,     KC_P6,   XXXXXXX,   KC_CAPS,   KC_LEFT,   KC_DOWN,   KC_UP,     KC_RIGHT,  KC_VOLD,  \
-  _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX, TG(_GAME),   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
+  _______,   KC_PDOT,   KC_P1,     KC_P2,     KC_P3,     KC_P0,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   KC_MUTE,  \
   _______,   _______,  _______,   _______,  _______,   _______,   KC_LEAD,   _______,   _______,   XXXXXXX,   XXXXXXX,   KC_ESC    \
-),
-
-/* Gaming
- * ,-----------------------------------------------------------------------------------.
- * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |  Up  |   O  |   P  |  #   |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |   A  |   S  |   D  |   F  |   G  |   H  | Left | Down |Right |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  =   | Ctrl | Alt  |Super |  NO  |    Space    |Enter | Alt  |  [   |TG(GAM)|C(Esc)|
- * `-----------------------------------------------------------------------------------'
- */
-[_GAME] = LAYOUT_preonic_grid( \
-  KC_ESC,    KC_1,      KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,        \
-  KC_TAB,    KC_Q,      KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_UP,   KC_O,    KC_P,    KC_NUHS,        \
-  KC_BSPC,   KC_A,      KC_S,    KC_D,    KC_F,    KC_G,    KC_H, KC_LEFT,    KC_DOWN, KC_RIGHT,KC_SCLN, KC_QUOT,        \
-  KC_LSFT,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,        \
-  KC_EQL,    KC_LCTL,   KC_LALT, KC_LGUI,_______,  KC_SPC,  KC_SPC,  KC_ENT,  KC_RALT, KC_LBRC, TO(_BASE), LCTL_T(KC_ESC)  \
-),
-
-/* Meta
-  ,-----------------------------------------------------------------------------------.
- * |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |  NO  |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |TG(META)|record|StpRec|PlayRec|  NO |    NO     |Speed+|Speed-|  NO  |RESET |MUSIC |
- * `-----------------------------------------------------------------------------------'
- */
-[_META] = LAYOUT_preonic_grid( \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,   XXXXXXX,  \
-  TO(_META), KC_LCTL,   KC_LALT,   KC_LGUI,   XXXXXXX,   XXXXXXX,   XXXXXXX,     KC_UP,   KC_DOWN,   XXXXXXX,     RESET,   MU_TOG    \
 ),
 
 };
@@ -123,8 +87,20 @@ LEADER_EXTERNS();
 
 void matrix_scan_user(void) {
 	LEADER_DICTIONARY() {
+		leading = false;
+		leader_end();
+
+		// leader -> r -> r
 		SEQ_TWO_KEYS(KC_L, KC_L) {
 			reset_keyboard();
+		}
+		// from bepo: leader -> k -> A
+		SEQ_TWO_KEYS(KC_B, KC_A) {
+			default_layer_set(_AZERTY);
+		}
+		// from azerty: leader -> k -> b
+		SEQ_TWO_KEYS(KC_K, KC_B) {
+			default_layer_set(_BEPO);
 		}
 	}
 }
