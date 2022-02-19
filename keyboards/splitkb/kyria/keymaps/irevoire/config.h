14ea2621d3d0d326c75150c41c8a78651a40f3fc
Tamo <tamo@meilisearch.com>
2022-02-20 00:12:23 +0100

    sync the state of the two screens

--- keyboards/splitkb/kyria/keymaps/irevoire/config.h
+++ keyboards/splitkb/kyria/keymaps/irevoire/config.h
@@ -30,3 +30,6 @@
 // If you are using an Elite C rev3 on the slave side, uncomment the lines below:
 // #define SPLIT_USB_DETECT
 // #define NO_USB_STARTUP_CHECK
+
+// sync the on-off state of the two screen
+#define SPLIT_OLED_ENABLE
